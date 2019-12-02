8f8507d442ab76fa18e82713c870a4a638d650f7
BriMarie <briannarodriguez0529@gmail.com>
2019-12-01 23:29:59 -0600

    Set up window to hold game graphics

---
+++ project/Gameplay.java
@@ -0,0 +1,31 @@
+package project;
+
+import javax.swing.JPanel;
+import java.awt.event.KeyListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Gameplay extends JPanel implements KeyListener, ActionListener {
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+    //TODO
+  }
+
+  @Override
+  public void keyTyped(KeyEvent e) {
+    //TODO
+  }
+
+  @Override
+  public void keyPressed(KeyEvent e) {
+    //TODO
+  }
+
+  @Override
+  public void keyReleased(KeyEvent e) {
+    //TODO
+  }
+
+}
