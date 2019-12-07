111311cec1994f7194277b44db6d55a68b55767e
BriMarie <briannarodriguez0529@gmail.com>
2019-12-06 21:15:34 -0600

    Create classes for other tetriminos

---
+++ project/J.java
@@ -0,0 +1,49 @@
+package project;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class J extends Tetromino {
+
+  private int xPos;
+  private int yPos;
+  public Color color = Color.BLUE;
+  public Block[] block = new Block[4];
+
+  public J() {
+    xPos = 5;
+    yPos = 0;
+    //TODO
+  }
+
+  @Override
+  public void moveDown() {
+    //TODO
+  }
+
+  @Override
+  public void moveLeft() {
+    //TODO
+  }
+
+  @Override
+  public void moveRight() {
+    //TODO
+  }
+
+  @Override
+  public void rotateLeft() {
+    //TODO
+  }
+
+  @Override
+  public void rotateRight() {
+    //TODO
+  }
+
+  @Override
+  public void draw(Graphics2D g) {
+    //TODO
+  }
+
+}
